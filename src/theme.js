6a18fbfcc1d38f175314b7748b972ce0e390f606
Paulo Freire <jose.freire@devoteam.com>
2020-05-14 23:12:12 +0100

    create theme

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  color: {
+    white: '#FFFFFF',
+    black: '#000000',
+    blue: '#73BCFF',
+    lightGray: '#EEEEEE',
+    Gray: '#666666',
+    darkGray: '#B3B3B3',
+  }
+}

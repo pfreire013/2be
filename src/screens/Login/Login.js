73771d923f4da0001ec42da8223766989bd0daed
Paulo Freire <jose.freire@devoteam.com>
2020-05-14 23:52:00 +0100

    start to add logic on Login

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import { Container, Title, LoginContainer, InputName, InputPassword, LoginButton, ButtonText } from './styles';
 
-function Login() {
+function Login(props) {
+  const [key, setKey ] = useState('')
+  const [senha, setSenha ] = useState('')
+  const history = useHistory();
+
+  const commonChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+        //nao entendi o que seria isso.. mas devemos criar um use state para capturar isso
+    });
+  }
+
+  const commonChangeModal = (e) => {
+    this.setState({ Modal: { ...this.state.Modal, [e.target.name]: e.target.value } })
+  }
+
+  const logarUsuario = (e) => {
+      e.preventDefault();
+
+      var _servicoAutenticacao = new Autenticacao();
+      try {
+          await _servicoAutenticacao.login(key, senha);
+          history.push('/home');
+      }
+      catch (error) {
+          return alert('Key ou senha inválidas')
+      }
+  }
+
+
+
   return (
     <Container>
         <LoginContainer>

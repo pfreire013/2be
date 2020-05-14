9b8afdab6d52fdec31012e6cbf06faa0562dfb89
Paulo Freire <jose.freire@devoteam.com>
2020-05-14 23:12:38 +0100

    atart to use global styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Routes from './routes';
+
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <div>
-        <p>
-          Home Login
-        </p>
-    </div>
-  );
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default App;

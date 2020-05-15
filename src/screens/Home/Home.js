5728333eed1e1aba1dbcec79e088fbd36ebfe8af
Paulo Freire <jose.freire@devoteam.com>
2020-05-15 16:49:05 +0100

    create base of layout on home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,61 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Title, TemplateContainer, TableHeader, TableBody, Subtille, TableContaier, ContactsContainer } from './styles';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <Title>Template</Title>
+      <TemplateContainer />
+      <Subtille>Contatos</Subtille>
+      <TableContaier>
+        <TableHeader>
+          <tr>
+            <th>
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+            </th>
+            <th scope="col">ID</th>
+            <th scope="col">Origem</th>
+            <th scope="col">Celular</th>
+            <th scope="col">Gerada</th>
+            <th scope="col">Processada</th>
+            <th scope="col">Status</th>
+          </tr>
+        </TableHeader>
+        <TableBody>
+          <td>
+          <input
+            name="isGoing"
+            type="checkbox"
+          />
+          </td>
+          <td>0000000</td>
+          <td>Interna</td>
+          <td>(13)996648775</td>
+          <td>01/03/2020</td>
+          <td>03/03/2020</td>
+          <td>Enviada</td>
+        </TableBody>
+        <TableBody>
+          <td>
+          <input
+            name="isGoing"
+            type="checkbox"
+          />
+          </td>
+          <td>0000000</td>
+          <td>Interna</td>
+          <td>(13)996648775</td>
+          <td>01/03/2020</td>
+          <td>03/03/2020</td>
+          <td>Enviada</td>
+        </TableBody>
+      </TableContaier>
+
+    </Container>
   );
 }
 
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+import { theme } from '../../theme'
+
+export const Container = styled.div`
+  background: ${theme.color.white};
+  border-radius: 20px;
+  max-width: 700px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Title = styled.h1`
+  font-size: 24px;
+  color: #000;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-bottom: 20px;
+`
+
+export const TemplateContainer = styled.div`
+  display: flex;
+  background: ${theme.color.white};
+  height: 150px;
+  width: 80%;
+  border: 2px solid ${theme.color.lightGray};
+  border-radius: 20px;
+  margin-bottom: 40px;
+`
+
+export const Subtille = styled.h1`
+  font-size: 20px;
+  color: #000;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const TableContaier = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: fixed;
+  margin-top: 20px;
+`
+
+export const TableHeader = styled.thead`
+
+  input {
+    font-size: 20px;
+  }
+
+  tr {
+    background: ${theme.color.lightGray};
+    height: 40px;
+    margin: 0;
+  }
+
+`
+
+export const TableBody = styled.tr`
+  height: 50px;
+  margin: 0;
+  border-bottom: 1px solid ${theme.color.lightGray};
+
+  input {
+    font-size: 20px;
+  }
+  
+  td {
+    text-align: center;
+  }
+`
+
+export const ContactsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`

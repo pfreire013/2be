5603963d2a0df63e2ffcccc08c6db98609a3914f
Paulo Freire <jose.freire@devoteam.com>
2020-05-16 02:12:48 +0100

    Add new routes

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Login from './screens/Login'
-import Home from './screens/Home'
-import Received from './screens/Received'
+import ReceivedMessages from './screens/ReceivedMessages'
+import Template from './screens/Template'
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Login}/>
-        <Route path='/home' component={Home}/>
-        <Route path='/received' component={Received}/>
+        <Route path='/received' component={ReceivedMessages}/>
+        <Route path='/template' component={Template}/>
       </Switch>
     </BrowserRouter>
   );
